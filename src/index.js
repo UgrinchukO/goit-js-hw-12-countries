1589fda64eba9b2dcf1c3873868705611297cc45
Oleg Ugrinchuk <ugrinchuko@gmail.com>
2020-07-30 15:20:03 +0300

    fixed styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import debounce from 'lodash.debounce';
-import './styles.css';
+import './styles.scss';
 import '../node_modules/toastr/build/toastr.css';
 import refs from './js/refs';
 import fetchCountries from './js/fetchCountries';
